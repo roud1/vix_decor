e074c6dae112408cd0f1e4e194bbd27baf8cef0b
roud1 <sh.s.roudi@gmail.com>
2022-06-13 19:10:38 +0300

    js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 
+    $('.slider').bxSlider({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        pager: false
+    });
+
     $('.owl-carousel').owlCarousel({
 
         loop:true,
         margin:0,
         nav:true,
         autoplay:true,
+        dotsData: true,
         navText: [ '', ' ' ],
-
         responsive:{
             0:{
                 items:1
@@ -17,8 +24,5 @@ $(document).ready(function(){
                 items:1
             }
         }
-
     });
 });
-
-
